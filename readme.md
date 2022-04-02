755c54635e15f4d89cdb3c0f553346e72bd55ba2
unknown <bensonmakau2000@gmail.com>
2022-04-02 11:06:55 +0300

    initialized js fundamentals part 2

---
+++ readme.md
@@ -0,0 +1,11 @@
+### Activating Strict Mode
+- `'use strict';` activating strict mode
+- Strict Mode:
+    - Forbids us from doing certain things
+    - Creates visible errors in the developer console.
+    - Introduces list of variable names that are reserved for features that may be added to the language later.
+
+### Functions
+- Its a piece of code that can be reused over and over again. 
+- Parameters are like variables in a function. They are defined once the function is called. 
+- They represent input data of the function. 
---
+++ script.js
@@ -0,0 +1,25 @@
+'use strict';//activating strict mode
+// let hasDriversLicense = false;
+// const passTest = true;
+// if(passTest) hasDriversLicense = true;
+// if(hasDriversLicense) console.log('I can Drive!!');
+
+//sample researved variables
+//const interface = 'audio';
+//const private = 534;
+
+//Functions
+function logger(){
+    //Function Body (code executed when the function is called)
+    console.log('My name is benson');
+}
+
+logger();//Invoking/ Running/ Calling Function
+function fruitProcessor(apples, oranges){
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5,0);
+console.log(appleJuice);
