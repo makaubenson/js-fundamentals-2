60ce1794e05afc0236c147cf726b28f1ab103249
unknown <bensonmakau2000@gmail.com>
2022-04-02 13:00:22 +0300

    updated functions

--- script.js
+++ script.js
@@ -16,10 +16,14 @@ function logger(){
 
 logger();//Invoking/ Running/ Calling Function
 function fruitProcessor(apples, oranges){
-    console.log(apples, oranges);
+    // console.log(apples, oranges);
     const juice = `Juice with ${apples} apples and ${oranges} oranges`;
     return juice;
 }
 
 const appleJuice = fruitProcessor(5,0);
 console.log(appleJuice);
+// console.log(fruitProcessor(5,0));
+
+const appleOrangeJuice = fruitProcessor(2,4);
+console.log(appleOrangeJuice);
