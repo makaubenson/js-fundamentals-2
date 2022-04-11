b75c79d4dcaed1841bdeaaff7eba78b100528a06
unknown <bensonmakau2000@gmail.com>
2022-04-11 06:53:02 +0300

    arrays coding challenge solution 2

--- script.js
+++ script.js
@@ -183,7 +183,7 @@ if(friends.includes('Michael')){
 }else{
     console.log("You have no friend named Michael");
 }
-*/
+
 //Arrays Coding Challenge
 function calcTip(bill){
     let tip = 0;
@@ -215,3 +215,15 @@ total.push(total1);
 total.push(total2);
 total.push(total3);
 console.log(`These are the totals ${total}`);
+*/
+//Method 2
+const calcTip = function(bill){
+    return bill >=50 && bill <= 300 ? bill = 0.15*bill : 0.20*bill;
+}
+//Arrow Function
+// const calcTip = bill => bill >=50 && bill <= 300 ? bill = 0.15*bill : 0.20*bill;
+
+const bills =[125,555,44];
+const tips = [calcTip(bills[0]),calcTip(bills[1]),calcTip(bills[2])];
+const totals = [bills[0]+tips[0],bills[1]+tips[1],bills[2]+tips[2]];
+console.log(bills, tips, totals);
