00ce8d98df35e03bfa4d36b540f760e9ac9b83c2
unknown <bensonmakau2000@gmail.com>
2022-04-11 14:34:13 +0300

    more examples on looping through arrays

--- script.js
+++ script.js
@@ -371,3 +371,11 @@ const benson = [
     types.push( typeof benson[i]);
  }
  console.log(types);
+ 
+ const years = [1991,2007,1969,2020];
+ const ages = [];
+ for(let i = 0; i < years.length; i++){
+     ages.push(2037- years[i]);
+ }
+
+ console.log(years, ages);
